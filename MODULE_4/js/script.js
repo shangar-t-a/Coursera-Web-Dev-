15501f7590084a2ddd587401105203442f24727c
shan <slrshan20@gmail.com>
2020-07-28 19:17:41 +0530

    Module 4

--- MODULE_4/js/script.js
+++ MODULE_4/js/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Shangar", "Jonas", "Gwen", "Ben", "Lucifer", "Noah", "Jackie Chan", "Bonnie Bennet", "Elena Gilbert", "Janson","Elijah Michaelson","Klaus Michaelson"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {

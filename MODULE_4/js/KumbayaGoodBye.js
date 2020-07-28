8a411200b4afd2c2ace91ecd15846c187a08c127
shangar-t-a <slrshan20@gmail.com>
2020-07-28 19:27:13 +0530

    Module 41

--- MODULE_4/js/KumbayaGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeAmigo = new Object();
-    var Greet = "Good Bye";
-    byeAmigo.speak = function speak(name) {
-        console.log(Greet + " " + name);
-    };
-    window.byeAmigo = byeAmigo;
-})(window);
--- MODULE_4/js/KumbayaHello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var helloAmigo = new Object();
-    var Greet = "Hello";
-    helloAmigo.speak = function(name) {
-        console.log(Greet + " " + name);
-    };
-    window.helloAmigo = helloAmigo;
-})(window);
